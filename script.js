ec7f531a7d6b1af814bf343eff6e9c040ba49fe6
Tomany <202348291+tomanyy@users.noreply.github.com>
2025-06-07 17:29:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,7 @@ document.getElementById('start-button').onclick = () => {
 };
 
 function launchApp(appName) {
-  const win = document.createElement('div');
-  win.classList.add('window');
-  win.style.top = Math.random() * 300 + 'px';
-  win.style.left = Math.random() * 300 + 'px';
-  win.innerHTML = `<iframe src="apps/${appName}.html"></iframe>`;
-  document.getElementById('window-container').appendChild(win);
+  const appUrl = `apps/${appName}.html`;
+  WindowManager.launch(appName, appUrl);
   document.getElementById('start-menu').classList.add('hidden');
 }
